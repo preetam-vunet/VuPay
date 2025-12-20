496743a21e12e9b53cf69545b3a2bd36bf19206f
preetam-vunet <preetamv@vunetsystems.com>
2025-12-20 12:19:18 +0530

    Minor chnges

--- app/otp-verification/page.tsx
+++ app/otp-verification/page.tsx
@@ -199,7 +199,7 @@ export default function OtpPage() {
                         )}
                     </div>
 
-                    {message && <p className={styles.message} style={{ marginTop: "1rem" }}>{message}</p>}
+                    {message && <p className={styles.message} style={{ marginTop: "1rem", color: "black" }}>{message}</p>}
                 </div>
             </div>
             <div className={styles.view}>
